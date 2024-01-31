969a17cb1f4c8af709c1bc806765d997cb593a31
ZEYAD_BIN_KUWAIR <zeyadmbk@gmail.com>
2024-01-31 21:49:03 +0400

    Adding clean changelog for Slack notification

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,11 @@ tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
 if [ "$tag" ]; then
   echo "Previous tag is ${tag}"
   changelog=$(git log --pretty=format:"%C(yellow)%h%Creset %ad [%an]  %s" --date=short $tag..HEAD)
+  slackchangelog=$(git log --pretty=format:"%ad [%an]  %s" --date=short $tag..HEAD)
 else
   echo "Previous tag is empty"
   changelog=$(git log --pretty=format:"%C(yellow)%h%Creset %ad [%an]  %s" --date=short)
+  slackchangelog=$(git log --pretty=format:"%ad [%an]  %s" --date=short)
 fi
 
 echo "Changelog is ### ${changelog}"
@@ -20,5 +22,10 @@ changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A' - }"
 changelog=" - ${changelog//$'\r'/'%0D'}"
 
+slackchangelog="${slackchangelog//'%'/'%25'}"
+slackchangelog="${slackchangelog//$'\n'/'%0A' - }"
+slackchangelog=" - ${slackchangelog//$'\r'/'%0D'}"
+
 # echo "::set-output name=changelog::$changelog"
-echo "changelog=$changelog" >> $GITHUB_OUTPUT
+echo "changelog=$slackchangelog" >> $GITHUB_OUTPUT
+echo "slackchangelog=$slackchangelog" >> $GITHUB_OUTPUT

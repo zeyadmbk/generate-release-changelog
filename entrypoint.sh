a2ffac9b79145177b40d4be7916a8a90858ca76b
ZEYAD_BIN_KUWAIR <zeyadmbk@gmail.com>
2024-01-31 20:34:31 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,14 @@ git config --global --add safe.directory /github/workspace
 tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
 
 if [ "$tag" ]; then
+  echo "Previous tag is ${tag}"
   changelog=$(git log --pretty=format:"%C(yellow)%h%Creset %ad [%an]  %s" --date=short $tag..HEAD)
 else
+  echo "Previous tag is empty"
   changelog=$(git log --pretty=format:"%C(yellow)%h%Creset %ad [%an]  %s" --date=short)
 fi
 
-echo $changelog
+echo "Changelog is ### ${changelog}"
 
 changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A' - }"

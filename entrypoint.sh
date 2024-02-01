3ffc264002e0ac14bc262d9cba2c22fe9a7d37c4
ZEYAD_BIN_KUWAIR <zeyadmbk@gmail.com>
2024-02-01 14:05:08 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,24 +8,24 @@ tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
 
 if [ "$tag" ]; then
   echo "Previous tag is ${tag}"
-  changelog=$(git log --pretty=format:"%C(yellow)%h%Creset %ad [%an]  %s" --date=short $tag..HEAD)
-  slackchangelog=$(git log --pretty=format:"%ad [%an]  %s" --date=short $tag..HEAD)
+  changelog=$(git log --pretty=format:":white_check_mark: %C(yellow)%h%Creset %ad [%an]:\n%B" --date=short $tag..HEAD)
+  slackchangelog=$(git log --pretty=format:":white_check_mark: %ad [%an]:\n%B" --date=short $tag..HEAD)
 else
   echo "Previous tag is empty"
-  changelog=$(git log --pretty=format:"%C(yellow)%h%Creset %ad [%an]  %s" --date=short)
-  slackchangelog=$(git log --pretty=format:"%ad [%an]  %s" --date=short)
+  changelog=$(git log --pretty=format:":white_check_mark: %C(yellow)%h%Creset %ad [%an]:\n%B" --date=short)
+  slackchangelog=$(git log --pretty=format:":white_check_mark: %ad [%an]:\n%B" --date=short)
 fi
 
 echo "Changelog is ### ${changelog}"
 
 changelog="${changelog//'%'/'%25'}"
-changelog="${changelog//$'\n'/'%0A' - }"
+# changelog="${changelog//$'\n'/'%0A' - }"
 changelog=" - ${changelog//$'\r'/'%0D'}"
 
 slackchangelog="${slackchangelog//'%'/'%25'}"
-slackchangelog="${slackchangelog//$'\n'/'%0A' - }"
+# slackchangelog="${slackchangelog//$'\n'/'%0A' - }"
 slackchangelog=" - ${slackchangelog//$'\r'/'%0D'}"
 
 # echo "::set-output name=changelog::$changelog"
-echo "changelog=$slackchangelog" >> $GITHUB_OUTPUT
+echo "changelog=$changelog" >> $GITHUB_OUTPUT
 echo "slackchangelog=$slackchangelog" >> $GITHUB_OUTPUT

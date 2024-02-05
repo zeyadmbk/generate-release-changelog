0b5f2c27901cce1f123b7f821bf61647044cf190
ZEYAD_BIN_KUWAIR <zeyadmbk@gmail.com>
2024-02-05 14:53:23 +0400

    Printing the last 6 tags

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,10 @@ git clone --quiet https://github.com/$REPO &> /dev/null
 git config --global --add safe.directory /github/workspace
 
 tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
+tags=$(git tag --sort version:refname | tail -n 6)
 
 if [ "$tag" ]; then
+  echo "Tags list is ${tags}"
   echo "Previous tag is ${tag}"
   changelog=$(git log --pretty=format:":white_check_mark: %C(yellow)%h%Creset %ad [%an]:\n%B" --date=short $tag..HEAD)
   slackchangelog=$(git log --pretty=format:":white_check_mark: %ad [%an]:\n%B" --date=short $tag..HEAD)
